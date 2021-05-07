187ec72eef7507cc0ccb2a6fddb8d021d2a4d542
MatejMeklav <matej.meklav@student.um.si>
2021-05-07 21:46:20 +0200

    Dodali smo uporabnik controler, in uporabnik model. Metode, ki smo jih dodali: dodaj, brisi, spremeni, registracija, prijava, pridobivanje vseh in enega uporabnika.

---
+++ controllers/uporabnik_controller.php
@@ -0,0 +1,86 @@
+<?php
+
+//kontroler za delo z uporabniki
+class uporabnik_controller {
+
+  public function index() {
+
+      //pridobimo vse uporabnike
+    $uporabniki = Uporabnik::vsi();
+
+      //TODO: PRIKAZ
+    //require_once('views/oglasi/index.php');
+  }
+
+    //akcija, ki prikaže enega uporabnika
+  public function prikazi() {
+     //preverimo, če id obstaja
+    if (!isset($_GET['id'])){
+        return call('strani', 'napaka'); //vrnemo napako v primeru, da id ne obstaja
+        //return prekine izvajanje nadaljnih korakov funkcije
+      }
+      //najdemo uporabnika
+      $uporabnik = Uporabnik::najdi($_GET['id']);
+
+    //TODO: PRIKAZ
+      //require_once('views/oglasi/prikazi.php');
+    }
+
+  //dodajanje uporabnikov, prikaz
+    public function dodaj() {
+      //TODO: Prikaz vmesnika za dodajanje uporabnika(login form)
+      //require_once('views/oglasi/dodaj.php');
+    }
+
+    public function registracija(){
+        $error = "";
+        if(isset($_POST["poslji"])){
+            /*
+              VALIDACIJA: preveriti moramo, ali je uporabnik pravilno vnesel podatke (unikatno uporabniško ime, dolžina gesla,...)
+              Validacijo vnesenih podatkov VEDNO izvajamo na strežniški strani. Validacija, ki se izvede na strani odjemalca (recimo Javascript),
+              služi za bolj prijazne uporabniške vmesnike, saj uporabnika sproti obvešča o napakah. Validacija na strani odjemalca ne zagotavlja
+              nobene varnosti, saj jo lahko uporabnik enostavno zaobide (developer tools,...).
+            */
+            //Preveri če se gesli ujemata
+            if($_POST["geslo"] != $_POST["ponovi_geslo"]){
+                $error = "Gesli se ne ujemata.";
+            }
+            //Preveri ali uporabniško ime obstaja
+            else if(Uporabnik::uporabnik_obstaja($_POST["uporabnisko_ime"])){
+                $error = "Uporabniško ime je že zasedeno.";
+            }
+            //Podatki so pravilno izpolnjeni, registriraj uporabnika
+
+            else if(Uporabnik::registracija($_POST["uporabnisko_ime"], $_POST["geslo"], $_POST["email"])){
+                //TODO pogled po registraciji
+                //require_once('views/strani/domov.php');
+            }
+            //Prišlo je do napake pri registraciji
+            else{
+                $error = "Prišlo je do napake med registracijo uporabnika.";
+            }
+
+        }else{
+            //TODO: Registracija pogled
+           // require_once('views/uporabnik/register.php');
+        }
+
+
+    }
+
+  //akcija shrani, pričakuje, da bo uporabnik poleg informacije o želeni akciji, preko POST metode, poslal tudi dva podatka - naslov in vsebino
+    public function shrani() {
+
+    //tukaj bi morali še preveriti vrednost uporabniškega vnosa glede varnosti in obstoja
+      //podatki so bili običajno posali in pogleda dodaj.php, ki ga je naložila akcija dodaj.
+      $uporabnik=Uporabnik::dodaj($_POST["uporabnisko_ime	"],$_POST["geslo"],$_POST["email"]);
+
+    //ko je oglas dodan, imamo v $oglas podatke o tem novem oglasu
+    //uporabniku lahko pokažemo pogled, ki ga bo obvestil o uspešnosti oddaje oglasa
+      require_once('views/oglasi/uspesnoDodal.php');
+    }
+
+
+
+  }
+  ?>
---
+++ models/Uporabnik.php
@@ -0,0 +1,144 @@
+<?php
+
+//model za oglas, ki vsebuje lasntosti, ki definirajo strukturo uporabnika
+//in metode, ki vračajo podatke iz trajne hrambe ali jih tja zapisujejo
+//v tem razredu so vse metode statične, lahko pa bi bile tudi običajne, pri čemer bi potrem bilo potrebno vsakič ustvarjat objekt
+
+
+class Uporabnik {
+  
+  public $id;
+  public $uporabnisko_ime;
+  public $geslo;
+  public $email;
+
+  //konstruktor
+  public function __construct($id, $uporabnisko_ime, $geslo,$email) {
+    $this->id = $id;
+    $this->uporabnisko_ime  = $uporabnisko_ime;
+    $this->geslo = $geslo;
+    $this->email=$email;
+  }
+
+
+    //metoda, ki iz baze vrne vse uporabnike
+  public static function vsi() {
+    $list = [];
+      //dobimo objekt, ki predstavlja povezavo z bazo
+    $db = Db::getInstance();
+      //izvedemo query
+    $result = mysqli_query($db,'SELECT * FROM uporabnik');
+
+//v zanki ustvarjamo nove objekte in jih dajemo v seznam
+    while($row = mysqli_fetch_assoc($result)){
+      $list[] = new Uporabnik($row['id'], $row['uporabnisko_ime'], $row['geslo'],$row['email']);
+    }
+    
+    //vrnemo list objektov/uporabnikov
+    return $list;
+  }
+
+
+
+  //metoda, ki vrne enega uporabnika s specifičnim id-jem iz baze
+  public static function najdi($id) {
+
+    $id = intval($id);
+    
+    $db = Db::getInstance();
+    $result = mysqli_query($db,"SELECT * FROM uporabnik where id=$id");
+    $row = mysqli_fetch_assoc($result);
+    return new Uporabnik($row['id'], $row['uporabnisko_ime'], $row['geslo'],$row['email']);
+  }
+  
+
+    //metoda, ki doda novega uporabnika v bazo
+
+  public static function dodaj($uporabnisko_ime,$geslo,$email) {
+    
+    $db = Db::getInstance();
+    
+	  //primer query-a s prepared statementom
+      $geslo_sha= sha1($geslo);
+
+    if ($stmt = mysqli_prepare($db, "Insert into Uporabnik (uporabnisko_ime,geslo,email) Values (?,?,?)")) {
+			//dodamo parametre po vrsti namesto vprašajev
+			//s string, i integer ,d double, b blob
+     mysqli_stmt_bind_param($stmt, "sss",$uporabnisko_ime,$geslo_sha,$email);
+     mysqli_stmt_execute($stmt);
+     mysqli_stmt_close($stmt);
+   }
+   
+   //dobimo nazaj informacijo o ID-ju, ki ga je generiral SQL strežnik
+   $id=mysqli_insert_id($db);
+   
+    //z uporabo metode najdi, najdemo uporabnika in ga vrnemo controlerju
+   return Uporabnik::najdi($id);
+ }
+
+
+    public static function prijava($uporabnisko_ime, $geslo){
+        $db = Db::getInstance();
+        $result = mysqli_query($db,"SELECT * FROM uporabnik WHERE uporabnisko_ime='".mysqli_real_escape_string ($db , $uporabnisko_ime)."' AND geslo='".mysqli_real_escape_string ($db , sha1($geslo))."';");
+
+        $row = mysqli_fetch_assoc($result);
+        if(!$row){
+            return false;
+        }else{
+            return new Uporabnik($row['id'], $row['uporabnisko_ime'], $row['geslo'],$row['email']);
+        }
+
+    }
+
+    public static function registracija($uporabnisko_ime, $geslo, $email){
+        ini_set('display_errors',1);
+        error_reporting(E_ALL);
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $db = Db::getInstance();
+        $admin = 0;
+        $geslo_sha= sha1($geslo);
+        if ($stmt = mysqli_prepare($db, "Insert into Uporabnik (uporabnisko_ime,geslo,email) Values (?,?,?)")) {
+            //dodamo parametre po vrsti namesto vprašajev
+            //s string, i integer ,d double, b blob
+            mysqli_stmt_bind_param($stmt, "sss",$uporabnisko_ime,$geslo_sha,$email);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+
+    }
+
+    public static function uporabnik_obstaja ($username){
+        $db =Db::getInstance();
+        $username = mysqli_real_escape_string($db, $username);
+        $result = mysqli_query($db,"SELECT * FROM user WHERE username='".$username."'");
+        $row = mysqli_fetch_assoc($result);
+        if(!$row){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public static function posodobi($data){
+        $db = Db::getInstance();
+
+        if ($stmt = mysqli_prepare($db, "UPDATE user SET uporabnisko_ime=?, geslo=?, email=? WHERE id=?")) {
+            //dodamo parametre po vrsti namesto vprašajev
+            //s string, i integer ,d double, b blob
+            mysqli_stmt_bind_param($stmt, "ssss",$data['uporabnisko_ime'],$data['geslo'] , $data['email'], $data['id']);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    public static function izbrisi($id){
+        $id_uporabnik = intval($id);
+        $db = Db::getInstance();
+        $result = mysqli_query($db,"DELETE FROM user WHERE id=$id_uporabnik");
+        //result, lahko preverimo uspešnost poizvedbe;
+    }
+
+
+}
+?>
